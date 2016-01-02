8df5a72275c48f3a054c0afe2707587f16cfec25
eloiqs <eloi.qs@outlook.com>
2016-01-02 17:59:44 -0500

    start implementing the contact conroller & view

---
+++ src/client/app/contact/contact.controller.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('LandingApp')
+        .controller('Contact', _contact);
+
+    _contact.$inject = [];
+
+    function _contact() {
+        /*jshint validthis: true */
+        var vm = this;
+
+    }
+})();
