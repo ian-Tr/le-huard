19617759d0143fd995d43ad0ebd558242df0f5b1
ian-Tr <ianthetremblay@gmail.com>
2016-02-07 01:08:48 -0500

    bugged forgot password back end

--- src/client/app/portfolio/forgot-password/forgot-password.controller.js
+++ src/client/app/portfolio/forgot-password/forgot-password.controller.js
@@ -5,12 +5,43 @@
         .module('Portfolio')
         .controller('ForgotPassword', forgotPassword);
 
-    forgotPassword.$inject = [];
+    forgotPassword.$inject = ['$http', '$state'];
 
-    function forgotPassword() {
+    function forgotPassword($http, $state) {
         /*jshint validthis: true */
         var vm = this;
 
+        vm.forgot = {};
+        vm.forgot.email = null;
+        vm.sendPassword = sendPassword;
+
+        function sendPassword(forgot) {
+          vm.showError = false;
+          vm.showSuccess = false;
+
+            if (vm.forgot.email !== null) {
+                $http.post('/src/server/forgotPassword/forgotPassword.php', forgot).then(function(response) {
+                    //http return success block
+                    var statusCode = response.status;
+                    if (statusCode === 201) {
+                        vm.showSuccess = true;
+                        //$state.go('portfolio.connection');
+                    }
+                }, function (response) {
+                  //http return error block
+                  var statusCode = response.status;
+                  if (statusCode === 409 || statusCode === 404) {
+                      vm.showError = true;
+                  }
+                });
+            }
+            else {
+              vm.showError = true;
+            }
+
+          //email field should get cleared either way
+          vm.forgot.email = null;
+        }
     }
 
 })();
---
+++ src/server/forgotPassword/forgotPassword.php
@@ -0,0 +1,81 @@
+<?php
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $request = file_get_contents('php://input');
+    $request = utf8_encode($request);
+    $forgot_request = json_decode($request, true);
+    $forgot = (array) $forgot_request;
+    echo "servr";
+    var_dump($forgot);
+    var_dump($forgot['email']);
+
+    if ($forgot['email'] !== null && $forgot['email'] !== '') {
+      $forgotEmail = $forgot['email'];
+      echo "nulls";
+      $db = new mysqli("localhost", "root", "admin", "le-huard");
+      if ($db -> connect_error) {
+        echo "db";
+          http_response_code(404);
+	    }
+      else {
+        echo "query";
+        //do this dank stuff
+        //look for email in db
+        var_dump($forgotEmail);
+        $find_email_query = $db -> query("SELECT * FROM member WHERE email = '".$forgotEmail."'") or die("Error: find_email_query");
+        if (mysqli_num_rows($find_email_query) > 0) {
+          echo "email found";
+          //email was found in db
+          $account = $find_email_query -> fetch_assoc();
+          $accountEmail = $account['email'];
+          $accountPassword =
+
+          $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+          $characterLength = strlen($characters);
+          $randomString = '';
+          for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+          }
+          var_dump($characterLength);
+          var_dump($randomString);
+
+          //construct mail parameters
+          $to = $alexEmail;
+          $from = $contact['email'];
+          $subject = $contact['subject'];
+          $message = $contact['message'];
+          $headers = array();
+          $headers[] = ('From: '.$from);
+          $headers[] = ('Bcc: '.$from);
+
+          /*
+          //send email
+          if (mail($to, $subject, $message, implode("\r\n", $headers))) {
+            //email sent
+            http_response_code(201);
+          }
+          else {
+            //server failed to send email
+            http_response_code(409);
+          }
+          */
+
+        }
+        else {
+          //email wasn't found in db
+          echo "not found";
+          http_response_code(404);
+        }
+      }
+    }
+    else {
+      //form data not found
+      http_response_code(404);
+    }
+  }
+  else {
+    //correct server request method not found
+    http_response_code(404);
+  }
+
+?>
